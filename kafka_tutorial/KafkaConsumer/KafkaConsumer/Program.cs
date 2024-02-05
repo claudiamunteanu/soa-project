7333fe0beff28abb807e6a0a124ec7d0aaf13277
claudiamunteanu <79506727+claudiamunteanu@users.noreply.github.com>
2024-02-05 23:29:45 +0200

    added tutorial

---
+++ kafka_tutorial/KafkaConsumer/KafkaConsumer/Program.cs
@@ -0,0 +1,52 @@
+﻿using Confluent.Kafka;
+
+namespace KafkaConsumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kafka consumer configuration
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = "localhost:9092",
+                GroupId = "groupId",
+            };
+
+            // Create a consumer instance
+            using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+            {
+                // Subscribe to a Kafka topic
+                consumer.Subscribe("tutorial");
+
+                Console.WriteLine("Consumer started. Press Ctrl+C to exit.");
+
+                // Start consuming messages
+                try
+                {
+                    while (true)
+                    {
+                        // Consume a message
+                        var consumeResult = consumer.Consume();
+
+                        // Check if the message was consumed successfully
+                        if (consumeResult != null)
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine(consumeResult.Message.Value);
+                            Console.WriteLine();
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    // Graceful shutdown if cancellation is requested
+                }
+                finally
+                {
+                    consumer.Close();
+                }
+            }
+        }
+    }
+}
---
+++ kafka_tutorial/KafkaProducer/KafkaProducer/Program.cs
@@ -0,0 +1,28 @@
+﻿using Confluent.Kafka;
+
+namespace KafkaProducer
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string bootstrapServers = "localhost:9092";
+            string topic = "tutorial";
+
+            var producerConfig = new ProducerConfig
+            {
+                BootstrapServers = bootstrapServers,
+            };
+
+            using (var producer = new ProducerBuilder<Null, string>(producerConfig).Build())
+            {
+                // Produce a message to the topic
+                var message = new Message<Null, string> { Value = "Hello, Kafka!" };
+                var deliveryReport = await producer.ProduceAsync(topic, message);
+
+                // Print delivery report details
+                Console.WriteLine($"Delivered message to {deliveryReport.TopicPartitionOffset}");
+            }
+        }
+    }
+}
